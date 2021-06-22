b4db6e616334fcce1488e07540e20bc0589244c5
Fabrício Silva Cardoso <fazinho1999@hotmail.com>
2021-06-22 16:54:14 -0300

    Leitura do arquivo

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main(){
-print("oi");
+int main(){
+
+FILE *fp;
+char c[200];
+char s[30];
+int i=0;
+
+while(1){
+   fscanf(fp,"%[^\n]",c);
+    if(feof(fp)){
+    break;
+    }
+    i++;
+}
+printf("%d",i);
+/*fp = fopen("List.txt","r");
+fscanf(fp,"%[^\n]",c);
+printf("%s",c);
+fclose(fp);*/
 
 return 0;
 
