14f7a38f000872cd8415aa210ea5aa26d807cbdd
Fabrício Silva Cardoso <fazinho1999@hotmail.com>
2021-06-24 17:24:50 -0300

    feat: Organiza na lista

--- main.c
+++ main.c
@@ -1,26 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
+typedef struct itemsList{
+    char item[20];
+   struct itemsList *next;
+} itemsList;
+
 FILE *fp;
-char c[200];
+char c[30];
 char s[30];
-int i=0;
+int totalItems = 0;
 
-while(1){
-   fscanf(fp,"%[^\n]",c);
-    if(feof(fp)){
-    break;
-    }
-    i++;
+char row[30];
+
+fp = fopen("List.txt","r");
+
+itemsList* itemList = NULL;
+itemList =(itemsList *)malloc(sizeof(itemsList));
+
+while(fgets(row,sizeof(row),fp) != NULL){
+
+    strcpy(itemList -> item, row);
+    totalItems++;   
 }
-printf("%d",i);
-/*fp = fopen("List.txt","r");
-fscanf(fp,"%[^\n]",c);
-printf("%s",c);
-fclose(fp);*/
 
+strcpy(itemList->item, row);
+printf("%s\n", itemList->item);
+itemList->next = NULL;
+
+
+/*printf("%s\n", itemList[0]);*/
+
+/*printf("%d\n",i);*/
+
+/*fseek(fp, 0, SEEK_SET); /* Return the file pointer to the beggining*/
+/*fscanf(fp,"%[^\n]",c); /* Read each line of the file*/
+
+/*
+printf("%s",c);
+printf("%d",i);
+*/
+
+
+fclose(fp);
 return 0;
 
 }
+
+/*int totalItems(FILE *file){
+
+
+    return 
+}*/
